e11a50e0452e776335b3fbf69b2f29f44b261979
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-18 18:03:08 -0700

    completed new student form

---
+++ hackbright-web.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template, redirect
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    # raise Exception('pause here, please')
+
+    
+    github = request.args.get('github', 'jhacks')
+    first, last, github = hackbright.get_student_by_github(github)
+    student_info = render_template('student_info.html', first=first, last=last, github=github)
+
+    return student_info
+    # return "%s is the GitHub account for %s %s" % (github, first, last)
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/new-student", methods=['POST'])
+def student_add():
+    """Add a student."""
+    new_student = request.form.get('new_student')
+
+    print new_student
+
+    # flash('The new student was successfully added!')
+
+    return render_template('new_student.html')
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
